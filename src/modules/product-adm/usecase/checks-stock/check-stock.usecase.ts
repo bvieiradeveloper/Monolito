daf6524aea3a7e38c1e9e2644c58d5c68c480716
Bruno Vieira <b.vieira@reply.com>
2022-10-21 20:30:03 -0300

    add CheckStock

---
+++ src/modules/product-adm/usecase/checks-stock/check-stock.usecase.ts
@@ -0,0 +1,22 @@
+import { CheckStockInputDto, CheckStockOutputDto } from './check-stock.dto';
+import ProductGateway from '../../gateway/product.gateway';
+export default class CheckStockUseCase{
+    
+    private repository: ProductGateway;
+    
+    constructor(repository: ProductGateway){
+        this.repository = repository;
+    }
+
+    async execute(input:CheckStockInputDto): Promise<CheckStockOutputDto>{
+
+        const product = await this.repository.find(input.productId);
+
+        console.log(product.id.id);
+        return {
+            productId: product.id.id,
+            stock: product.stock,
+        };
+    }
+
+}
